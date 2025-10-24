5e28c629169653651a2a6d1c11f73a1fe7a39e7f
addour <mt.kaddour@gmail.com>
2025-10-24 14:22:59 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+(function () {
+  'use strict';
+
+  angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+  
+  function LunchCheckController($scope) {
+    $scope.lunchItems = "";
+    $scope.message = "";
+
+    $scope.checkLunch = function () {
+      if ($scope.lunchItems.trim() === "") {
+        $scope.message = "Please enter data first";
+      } else {
+        var items = $scope.lunchItems.split(',');
+        var count = items.length;
+        
+        if (count <= 3) {
+          $scope.message = "Enjoy!";
+        } else {
+          $scope.message = "Too much!";
+        }
+      }
+    };
+  }
+
+})();
